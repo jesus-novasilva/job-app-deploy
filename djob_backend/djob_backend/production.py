27ace5acc8ec53f0e6af3406031e611874ca72c3
jesus-novasilva <jesus.villalon@novasilva.com>
2024-07-01 14:25:57 +0200

    production.py update

--- djob_backend/djob_backend/production.py
+++ djob_backend/djob_backend/production.py
@@ -8,6 +8,7 @@ from .settings import BASE_DIR
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 DEBUG = False
+SECRET_KEY = os.environ['MY_SECRET_KEY']
 
 # WhiteNoise configuration
 MIDDLEWARE = [
