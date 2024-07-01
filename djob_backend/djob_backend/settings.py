6d4148c758a09c6990c2cc33c3e29612ac221c41
jesus-novasilva <jesus.villalon@novasilva.com>
2024-07-01 17:23:02 +0200

    backend changes updated

--- djob_backend/djob_backend/settings.py
+++ djob_backend/djob_backend/settings.py
@@ -87,10 +87,21 @@ WSGI_APPLICATION = 'djob_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DBNAME'),
+        'HOST': os.environ.get('DBHOST'),
+        'USER': os.environ.get('DBUSER'),
+        'PASSWORD': os.environ.get('DBPASS'),
+        'PORT': '5432',
     }
 }
 
